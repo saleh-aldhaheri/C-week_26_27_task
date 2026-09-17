e7595609c190bd468994937b3e5208c028f1647f
saleh-aldhaheri <salehaldhaheri09@gmail.com>
2026-09-17 15:25:14 +0800

    feat: setup identify feature structure

---
+++ week_26_27_task/Areas/Identity/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace week_26_27.Areas.Identity.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
